d8bd14602c221668a0f12be840e5ad244151dc31
sagegod <cdeng1999@gmail.com>
2022-06-23 17:49:50 -0700

    added employee and intern functions

--- index.js
+++ index.js
@@ -38,8 +38,128 @@ const init = () => {
                 answers.officeNumber
             )
             teamMemberObjectArr.push(manager)
+            addEmployees();
         })
     };
+
+    function addEmployees () {
+        inquirer
+        .prompt([  
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'What employees would you like to add?',
+            choices: ['Engineer', 'Intern', "I'm Done"]
+        },
+    ])
+
+    .then(answer => {
+        switch (answer.choice) {
+            case 'Engineer':
+                createEngineer ()
+                break;
+
+            case 'Intern':
+                createIntern ()
+                break;
+
+            case "I'm Done":
+                buildTeam()
+                break;
+        }
+    });
+    
+    function createEngineer () {
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is the Engineers ID?",
+            },
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your Engineers name?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is your Engineers email?',
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'What is your Engineers GitHub?',
+            },
+        ])
+        .then(answers => {
+            const engineer = new Engineer(
+                answers.id,
+                answers.name,
+                answers.email,
+                answers.github
+            )
+            teamMemberObjectArr.push(engineer)
+            addEmployees();
+        })
+
+    };
+
+    function createIntern () {
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is the Interns ID?",
+            },
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your Interns name?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is your Interns email?',
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: 'What school does your Intern go to?',
+            },
+        ])
+        .then(answers => {
+            const interns = new Interns(
+                answers.id,
+                answers.name,
+                answers.email,
+                answers.school
+            )
+            teamMemberObjectArr.push(interns)
+            addEmployees();
+        })
+    };
+
+    
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+    createManager()
 };
 
 init ()
