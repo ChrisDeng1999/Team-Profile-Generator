98af6d9dcfbeb902cb60275928d507102dae0ee0
sagegod <cdeng1999@gmail.com>
2022-06-25 23:30:09 -0700

    fixed some grammer issues

--- src/html-template.js
+++ src/html-template.js
@@ -89,7 +89,7 @@ module.exports = team => {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link href = "../assets/css/style.css" type="text/css" rel = "stylesheet">
 
-    <title>Team Memeber Generator</title>
+    <title>Team Member Generator</title>
 </head>
 <body>
   <header class = "d-flex align-items-center justify-content-center bg-danger text-white fs-1 mb-5" style = "height: 13vh">My Team</header>
